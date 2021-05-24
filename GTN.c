187bc0c2844fe458e15be9226335a5f1dd303753
Nikit-370 <81354106+Nikit-370@users.noreply.github.com>
2021-05-24 14:41:17 +0530

    Create GTN.c

---
+++ GTN.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int num;
+    int guess, nguess = 1;
+    srand(time(0));
+    num = rand() % 100 + 1;
+
+    do
+    {
+        printf("Guess a number between 1 to 100\n");
+        scanf("%d", &guess);
+
+        if (guess > num)
+        {
+            printf("Enter lesser number.\n");
+        }
+        else if (guess < num)
+        {
+            printf("Enter greater number.\n");
+        }
+        else
+        {
+            printf("Congratulations!! You got the number in %d attemps.\n", nguess);
+            getch();
+        }
+        nguess++;
+    } while (guess != num);
+
+    return 0;
+}
